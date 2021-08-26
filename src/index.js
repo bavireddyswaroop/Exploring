0fb303f1d69ab955cf5453dfca0d4e1176ffee26
bavireddyswaroop <84021737+bavireddyswaroop@users.noreply.github.com>
2021-08-26 11:50:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import eff from "./eff";
+import "./styles.css";
+
+function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState(number1 + number2);
+  const [total1, setTotal1] = useState(number1 + number2);
+  const [total2, setTotal2] = useState(number1 + number2);
+
+  function calculateTotal() {
+    setTotal(number1 + number2);
+  }
+  function calculateTotal1() {
+    setTotal(number1 - number2);
+  }
+  function calculateTotal2() {
+    setTotal(number1 * number2);
+  }
+
+  return (
+    <div className="App">
+      <h1>Add subtract multiply</h1>
+
+      <div className="number-inputs">
+        <input
+          type="number"
+          value={number1}
+          onChange={(e) => setNumber1(+e.target.value)}
+          placeholder="0"
+        />
+        <input
+          type="number"
+          value={number2}
+          onChange={(e) => setNumber2(+e.target.value)}
+          placeholder="0"
+        />
+      </div>
+
+      <button onClick={calculateTotal}>Add Them!</button>
+
+      <button onClick={calculateTotal}>subtract Them!</button>
+
+      <button onClick={calculateTotal}>multiply Them!</button>
+      <h2>{total}</h2>
+      <h2>{total}</h2>
+
+      <h2>{total}</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
